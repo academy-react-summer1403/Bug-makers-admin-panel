fde305374c909e12901d4d523ec4a2d23f64016c
Mohammad sd <mamdbio1386.com>
2024-11-29 12:24:31 +0330

    toast

--- src/@core/interceptor/interceptor.js
+++ src/@core/interceptor/interceptor.js
@@ -28,91 +28,28 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
   (response) => {
     if (response.data.success) {
-      const myPromise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (Math.random() < 0.5) {
-            resolve('foo')
-          } else {
-            reject('fox')
-          }
-        }, 1000)
-      })
-      toast.promise(
-        myPromise,
-         {
-           loading: 'در حال پردازش',
-           success: <b>عملیات با موفقیت انجام شد</b>,
-           error: <b>خطا</b>,
-         }
-       );    }
-    if (response.data.status === 200) {
-      const myPromise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (Math.random() < 0.5) {
-            resolve('foo')
-          } else {
-            reject('fox')
-          }
-        }, 1000)
-      })
-      toast.promise(
-        myPromise,
-         {
-           loading: 'در حال پردازش',
-           success: <b>عملیات با موفقیت انجام شد</b>,
-           error: <b>خطا</b>,
-         }
-       ); 
-    }
+      toast.success(response.data.message || 'عملیات با موفقیت انجام شد')
+    }     
+    if (response.data.success) {
+      toast.success(response.data.message || 'عملیات با موفقیت انجام شد')
+    }  
     return response;
   },
   
   (error) => {
     if (error.response && error.response.status === 422) {
       const errorMessage = error.response.data.ErrorMessage || "خطا: ورودی نامعتبر.";
+      toast.error(errorMessage || 'با خطا مواجه شدید')
 
-      Swal.fire({
-        title: 'با خطا مواجه شدید',
-        text: errorMessage,
-        icon: 'error',
-        timer: 3000,
-        timerProgressBar: true,
-        showCloseButton: false,
-        showConfirmButton: false,
-        customClass: {
-          timerProgressBar: 'bg-danger',
-        }
-      });
     }
     if (error.response.status === 401) {
-      Swal.fire({
-        title: 'با خطا مواجه شدید',
-        text: 'ابتدا وارد حساب کاربری خود شوید',
-        icon: 'error',
-        timer: 3000,
-        timerProgressBar: true,
-        showCloseButton: false,
-        showConfirmButton: false,
-        customClass: {
-          timerProgressBar: 'bg-danger',
-        }
-      });
+      toast.error(errorMessage || 'ابتدا وارد حساب کاربری خود شوید')
+
     }
     if (error.response.status === 403) {
       const errorMessage = error.response.data.ErrorMessage || "خطا: ورودی نامعتبر.";
+      toast.error(errorMessage || 'با خطا مواجه شدید')
 
-      Swal.fire({
-        title: 'با خطا مواجه شدید',
-        text: errorMessage,
-        icon: 'error',
-        timer: 3000,
-        timerProgressBar: true,
-        showCloseButton: false,
-        showConfirmButton: false,
-        customClass: {
-          timerProgressBar: 'bg-danger',
-        }
-      });
     }
     return Promise.reject(error);
   }
